543551cf0dc81dc6f5e39d99aecc34783250b070
Sean Clifford <sclifford6262@gmail.com>
2018-05-04 22:35:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function findMatching(drivers, string){
 }
 
 function fuzzyMatch(drivers, string){
-  return drivers.filter(function(driver){return driver.toLowerCase()
+  return drivers.filter(function(driver){return driver.toLowerCase()}
 }
