22d7e112fa5d9bbaab96d2dbb543facab97be3d9
Sean Clifford <sclifford6262@gmail.com>
2018-05-04 22:37:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function findMatching(drivers, string){
 function fuzzyMatch(drivers, string){
   return drivers.filter(function(driver){return driver.toLowerCase().slice(0,2) == string.toLowerCase()})
 }
+
+function matchName(drivers, string){
+  return drivers.filter()
+}
